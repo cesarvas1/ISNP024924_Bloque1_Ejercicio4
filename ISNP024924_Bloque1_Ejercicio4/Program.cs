b581bdb90e421565d981481de9c115e0923bf749
César <Csar@DESKTOP-4INIV8U>
2024-10-20 18:09:48 -0600

    Agregar archivos de proyecto.

---
+++ ISNP024924_Bloque1_Ejercicio4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero;
+
+        Console.Write("Ingrese un número para verificar si es primo: ");
+        numero = Convert.ToInt32(Console.ReadLine());
+
+        if (EsPrimo(numero))
+        {
+            Console.WriteLine("El número " + numero + " es primo.");
+        }
+        else
+        {
+            Console.WriteLine("El número " + numero + " no es primo.");
+        }
+
+        Console.WriteLine("Presione cualquier tecla para salir.");
+        Console.ReadKey();
+    }
+
+    static bool EsPrimo(int num)
+    {
+        if (num <= 1) return false;
+
+        for (int i = 2; i <= Math.Sqrt(num); i++)
+        {
+            if (num % i == 0) return false;
+        }
+
+        return true;
+    }
+}
